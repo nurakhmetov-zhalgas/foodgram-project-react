7dcf9ffa205526f7ee10c8f6d9f708a9cbe0b7f6
Zhalgas <nurakhmetov997@yandex.ru>
2022-05-16 10:49:49 +0300

    Users admin panel

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.forms import UserChangeForm, UserCreationForm
+from users.models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+    list_display = (
+        'email', 'is_staff', 'is_active',
+    )
+    list_filter = (
+        'is_staff', 'is_active'
+    )
+    fieldsets = (
+        (None, {
+            'fields': (
+                'username', 'first_name', 'last_name', 'email', 'password',
+            )
+        }),
+        ('Права', {
+            'fields': (
+                'is_staff', 'is_active',
+            )
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username', 'first_name', 'last_name', 'email', 'password1',
+                'password2', 'is_staff', 'is_active')}
+         ),
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+
+
+admin.site.register(User, CustomUserAdmin)
