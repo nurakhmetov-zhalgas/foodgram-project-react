497690546c73cf42328bd1b0b07b5b49a9e0ec0f
Zhalgas <nurakhmetov997@yandex.ru>
2022-05-16 12:22:42 +0300

    paginations, "limit" query param added

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -135,14 +135,14 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly', 
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "DEFAULT_PAGINATION_CLASS": "recipes.paginations.CustomPageNumberPagination",
+    "PAGE_SIZE": 6,
 
 }
 
